de2b76bea8f6bd79cee590b7b145f4950ec17c3d
derya <lacin>
2024-11-29 17:46:06 +0100

    deneme

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >1) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
